ee6fddbe5cea463888d2f3dd6187ed90a310bd67
boboman13 <rich@safe-node.com>
2014-02-06 17:07:46 -0500

    Adding css assets

---
+++ server/controllers/asset.js
@@ -0,0 +1,27 @@
+// index.js is a controller
+var fs = require("fs");
+
+module.exports = {
+	// run the controller
+	run: function(request, response) {
+		// write header
+		response.writeHeader(200, {'Content-Type': 'text/css'});
+
+		// read /static/network.html
+		fs.readFile(__dirname + '/../../static/asset/watcher.css', function(err, data) {
+			if (err) {
+				console.log('Error occured: could not access file ~/static/asset/watcher.css');
+				response.end();
+				return;
+			}
+
+			console.log(data);
+			response.write(data);
+
+			response.end();
+		});
+
+		// end
+		response.end();
+	}
+};
