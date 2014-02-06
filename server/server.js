e6b405720b906a0964e0028049204c6c84d12737
boboman13 <rich@safe-node.com>
2014-02-06 01:13:12 -0500

    Changes

--- server/server.js
+++ server/server.js
@@ -2,35 +2,20 @@
 
 var http = require("http");
 var url  = require("url");
-var fs   = require("fs");
 
-var routes = require("./routes");
+var routes = require("./routes").routes;
 
 function onRequest(request, response) {
-	var path = request.url;
+	var controller, path = request.url;
 
 	// check if a route exists for this path
-	if(routes.routes[path]) {
-		var controller = require("./controllers/" + routes.routes[path].controller);
+	if(routes[path]) {
+		controller = require("./controllers/" + routes[path].controller);
 		controller.run(request, response);
 	} else {
 		// route not defined. 404
-		response.writeHeader(404, {'Content-Type': 'text/html'});
-
-		// load ./static/404.html
-		fs.readFile(__dirname + '/../static/404.html', function(err, data) {
-			if (err) {
-				// did someone remove the page or something
-				console.log('Error occured: could not access file ~/static/404.html');
-				response.end();
-				return;
-			}
-
-			response.write(data);
-
-			response.end();
-		});
-
+		controller = require("./controllers/" + routes['404'].controller);
+		controller.run(request, response);
 	}
 }
 
