a89cc5f46243506aea237c3848487fd84aebb142
boboman13 <rich@safe-node.com>
2014-03-19 23:48:45 -0400

    Adds daemon

--- src/daemon/server.js
+++ src/daemon/server.js
@@ -1,8 +1,14 @@
 // daemon server.js
 var net = require("net");
+var stat = require("../stat/retrieve");
 
 var server = net.createServer(function(socket) {
-	
+	// get data
+	stat.stat(function(data) {
+		var json = JSON.stringify(data);
+		socket.write(json);
+		socket.end();
+	});
 });
 
 server.listen(3137, '0.0.0.0');
