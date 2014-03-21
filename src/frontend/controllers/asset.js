e192a43d5cba0137fcf70c77271a0aae734974e0
boboman13 <rich@safe-node.com>
2014-03-20 20:26:28 -0400

    Adds support for more assets

--- src/frontend/controllers/asset.js
+++ src/frontend/controllers/asset.js
@@ -1,23 +1,37 @@
-// index.js is a controller
+// asset.js is a controller
 var fs = require("fs");
+var url = require("url");
+var path = require("path");
 
 module.exports = {
 	// run the controller
 	run: function(request, response) {
-		// write header
-		response.writeHeader(200, {'Content-Type': 'text/css'});
+		// get the file uri
+		var uri = url.parse(request.url).pathname;
+		var path = "static/asset/" + uri.substring(7, uri.length);
 
-		// read /static/network.html
-		fs.readFile(__dirname + '/../../../static/asset/watcher.css', function(err, data) {
+		// check if path contains '..'
+		if(path.indexOf('..') > -1) {
+			response.writeHeader(500, {'Content-Type': 'text/plain'});
+			response.write("Error: Path cannot contain '..'.");
+			response.end();
+			return;
+		}
+
+		// check if file exists
+		fs.readFile(path, {encoding: 'utf8'}, function(err, data) {
 			if (err) {
-				console.log('Error occured: could not access file ~/static/asset/watcher.css');
+				response.writeHeader(404, {'Content-Type': 'text/plain'});
+				response.write('404: Asset not found');
 				response.end();
 				return;
 			}
 
+			response.writeHead(200);
 			response.write(data);
-
 			response.end();
+
+			return;
 		});
 	}
 };
